b7c59382f118b4a0362caf5ee2c14b8024d09a0a
AntoineGourtayHetic <antoine.gourtay@hetic.net>
2017-12-18 16:20:37 +0100

    :construction: Building app architecture

--- app/src/main/java/com/antoinegourtay/mob_e16_android/CryptoPlace.java
+++ app/src/main/java/com/antoinegourtay/mob_e16_android/CryptoPlace.java
@@ -2,10 +2,24 @@ package com.antoinegourtay.mob_e16_android;
 
 import android.app.Application;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by antoinegourtay on 18/12/2017.
  */
 
-public class CryptoPlace extends Application{
+public class CryptoPlace extends Application
 
+        private RequestQueue requestQueue;
+
+        @Override
+        public void onCreate() {
+            super.onCreate();   
+            requestQueue = Volley.newRequestQueue(this);
+        }
+
+        public RequestQueue getRequestQueue() {
+            return requestQueue;
+        }
 }
