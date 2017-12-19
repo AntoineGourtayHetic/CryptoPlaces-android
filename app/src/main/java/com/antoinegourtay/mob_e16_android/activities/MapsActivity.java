b0f92a81a0b8637187d1c8eddfb00f96369fd2d9
AntoineGourtayHetic <antoine.gourtay@hetic.net>
2017-12-19 16:01:21 +0100

    Adding markers to map

--- app/src/main/java/com/antoinegourtay/mob_e16_android/activities/MapsActivity.java
+++ app/src/main/java/com/antoinegourtay/mob_e16_android/activities/MapsActivity.java
@@ -10,14 +10,26 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.antoinegourtay.mob_e16_android.CryptoPlaceApplication;
 import com.antoinegourtay.mob_e16_android.R;
+import com.antoinegourtay.mob_e16_android.response.CryptoValueResponse;
+import com.antoinegourtay.mob_e16_android.response.PlacesResponse;
+import com.antoinegourtay.mob_e16_android.response.SinglePlaceResponse;
 import com.fasterxml.jackson.core.JsonParser;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.neopixl.spitfire.listener.RequestListener;
+import com.neopixl.spitfire.request.BaseRequest;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -73,9 +85,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 15));
                     onLaunch = false;
                 }
-
-                //TODO: Make the request to get the place using crypto currencies
-
 
             }
 
@@ -143,5 +152,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        //Request to get all the places from the API
+        BaseRequest<PlacesResponse> request =
+                new BaseRequest.Builder<>(Request.Method.GET
+                        , "https://api.myjson.com/bins/wfh57"
+                        , PlacesResponse.class)
+                        .listener(new RequestListener<PlacesResponse>() {
+                            @Override
+                            public void onSuccess(Request request, NetworkResponse response, PlacesResponse result) {
+                                Toast.makeText(MapsActivity.this, "OK", Toast.LENGTH_SHORT).show();
+
+                                for (SinglePlaceResponse singlePlaceResponse : result.getResults()) {
+                                    LatLng markerLatLng = new LatLng(
+                                            Double.parseDouble(singlePlaceResponse.getPosition().getLatitude()),
+                                            Double.parseDouble(singlePlaceResponse.getPosition().getLongitude())
+                                    );
+
+                                    mMap.addMarker(
+                                            new MarkerOptions()
+                                                    .title(singlePlaceResponse.getName())
+                                                    .position(markerLatLng)
+                                    );
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Request request, NetworkResponse response, VolleyError error) {
+                                Toast.makeText(MapsActivity.this, "FAIL", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .build();
+
+
+        CryptoPlaceApplication cryptoPlaceApplication =
+                (CryptoPlaceApplication) getApplication();
+        cryptoPlaceApplication.getRequestQueue().add(request);
     }
 }
