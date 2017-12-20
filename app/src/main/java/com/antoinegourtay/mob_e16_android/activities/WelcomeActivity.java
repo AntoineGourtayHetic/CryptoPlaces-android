5a4591a38d99b1e216323f786d8d3eac4b86a24a
AntoineGourtayHetic <antoine.gourtay@hetic.net>
2017-12-20 17:57:45 +0100

    creating first onboarding activity

---
+++ app/src/main/java/com/antoinegourtay/mob_e16_android/activities/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.antoinegourtay.mob_e16_android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.antoinegourtay.mob_e16_android.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
