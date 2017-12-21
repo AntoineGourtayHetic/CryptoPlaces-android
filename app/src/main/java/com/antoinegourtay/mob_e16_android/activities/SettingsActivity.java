1b05309afcf627deabb5f22ca7fb23d6a6863c32
AntoineGourtayHetic <antoine.gourtay@hetic.net>
2017-12-21 14:48:32 +0100

    updating project and making page setting work

--- app/src/main/java/com/antoinegourtay/mob_e16_android/activities/SettingsActivity.java
+++ app/src/main/java/com/antoinegourtay/mob_e16_android/activities/SettingsActivity.java
@@ -1,15 +1,33 @@
 package com.antoinegourtay.mob_e16_android.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.antoinegourtay.mob_e16_android.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTextPublicKeySettings)
+    EditText editTextPublickeySettings;
+
+    @BindView(R.id.buttonValidatePublickeyUpdate)
+    Button buttonValidatePublickeyUpdate;
+
+    SharedPreferences preferences;
+
+    private final String PREF_FILE_NAME = "my_preferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +40,12 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this);
+
+        preferences = getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+
+        editTextPublickeySettings.setText(preferences.getString("public_key", null));
+
     }
 
 
@@ -37,5 +53,15 @@ public class SettingsActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @OnClick(R.id.buttonValidatePublickeyUpdate)
+    void updatePublicKey() {
+        getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE)
+                .edit()
+                .putString("public_key", editTextPublickeySettings.getText().toString())
+                .apply();
+
+        Toast.makeText(SettingsActivity.this, "New key is set : " + editTextPublickeySettings.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
